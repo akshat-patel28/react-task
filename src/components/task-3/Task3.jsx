e821d75b78cefea5465dd954d498acc2486c2eec
Akshat Patel <patelakshat335@gmail.com>
2022-09-05 21:48:48 +0530

    task 3 done

--- src/components/task-3/Task3.jsx
+++ src/components/task-3/Task3.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Task3 = () => {
-  return <div>Task3</div>;
+  const list = [
+    { id: 1, title: "task1" },
+    { id: 2, title: "task2" },
+    { id: 3, title: "task3" },
+    { id: 4, title: "task4" },
+  ];
+  return (
+    <div>
+      <h1>Display a list</h1>
+      {list.map((element, index) => (
+        <div
+          key={index}
+          style={{
+            width: "80px",
+            height: "40px",
+            border: "1px solid black",
+            margin: "0 auto",
+          }}
+        >
+          <p>
+            {element.id} - {element.title}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Task3;
