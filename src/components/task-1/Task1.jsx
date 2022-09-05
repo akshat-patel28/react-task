02aa54228400c61c17c2de33f6432c7de987e68e
Akshat Patel <akshat.patel@globalvoxinc.com>
2022-09-05 17:19:24 +0530

    task 1 done

--- src/components/task-1/Task1.jsx
+++ src/components/task-1/Task1.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+const list = ["Ani", "Sam", " Joe", "Apple", "Banana", "Orange"];
 const Task1 = () => {
-  return <div>Task1</div>;
+  const [data, setData] = useState(list);
+
+  useEffect(() => {}, [data]);
+
+  const filterData = (e) => {
+    setData(
+      list.filter((element) =>
+        element.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+      )
+    );
+  };
+  return (
+    <div>
+      <h1> Search Filter</h1>
+      <input
+        type="text"
+        placeholder="search..."
+        onChange={(e) => filterData(e)}
+      />
+      {data.map((element) => (
+        <div>{element}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Task1;
