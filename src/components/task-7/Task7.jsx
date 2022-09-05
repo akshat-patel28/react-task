f901a95f2cecdae8770516b1b94280ee17cbe5fb
Akshat Patel <patelakshat335@gmail.com>
2022-09-05 22:06:59 +0530

    task 7 done

--- src/components/task-7/Task7.jsx
+++ src/components/task-7/Task7.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BASE_URL, GET_POST } from "../helper/constant";
 
 const Task7 = () => {
-  return <div>Task7</div>;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(BASE_URL + GET_POST)
+      .then((res) => {
+        if (res.status === 200) setData(res.data);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (loading) {
+    return <h1>Loading....</h1>;
+  }
+  return (
+    <div>
+      <h1>Data From Rest API</h1>
+      {data.map((element, index) => (
+        <p key={index}>{element.title}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Task7;
