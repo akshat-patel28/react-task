f6c278744384f68abb49e154268d1572419d2771
Akshat Patel <akshat.patel@globalvoxinc.com>
2022-09-06 11:01:13 +0530

    task 6 done

--- src/components/task-6/Task6.jsx
+++ src/components/task-6/Task6.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Task6 = () => {
-  return <div>Task6</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const onClick = () => {
+    setErrorMessage("");
+    const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email === "") setErrorMessage("email field is empty");
+    else if (password === "") setErrorMessage("password field is empty");
+    else if (email === "" && password === "") setErrorMessage("fill all field");
+    else if (email.match(emailFormat) === null)
+      setErrorMessage("invalid email address");
+    else {
+      setErrorMessage("");
+      alert("login successful");
+    }
+  };
+  return (
+    <div>
+      <h1>Login Form</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "200px",
+          margin: "0 auto",
+        }}
+      >
+        <input
+          type="email"
+          placeholder="Email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={onClick}>Login</button>
+        <span style={{ color: "red" }}>{errorMessage}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Task6;
